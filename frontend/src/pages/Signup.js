c69b477f73d585b4691f2bfda0fdc6fb3803f6cf
Aman Roy <86224475+Amannroy@users.noreply.github.com>
2024-11-11 18:35:40 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -30,7 +30,7 @@ const Signup = () => {
             return handleError('name, email and password are required');
         }
         try{
-           const url = "http://localhost:8080/auth/signup";
+           const url = "https://mern-auth-app-ectq.vercel.app/auth/signup";
            const response = await fetch(url,{
             method: "POST",
             headers: {
